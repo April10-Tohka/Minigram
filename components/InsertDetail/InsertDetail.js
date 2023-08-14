94aa3839c79ba1dee9a958f21982767ce1ea1451
Tohka <1324419148@qq.com>
2023-08-14 23:19:55 +0800

    实现了按钮选中样式改变；实现了数据的缓存效果

--- components/InsertDetail/InsertDetail.js
+++ components/InsertDetail/InsertDetail.js
@@ -1,5 +1,17 @@
 let Cachenum=1;//记录选择数量，实现数据缓存效果
 let CachefileList=[];//记录上传照片，实现数据缓存效果
+let CacheinsertList=[
+    {name:"蟑螂",ischoose:false,_id:"001"},
+    {name:"蜘蛛",ischoose:false,_id:"002"},
+    {name:"老鼠",ischoose:false,_id:"003"},
+];
+let CachesizeList=[
+    {name:"大",ischoose:false,_id:"001"},
+    {name:"中等",ischoose:false,_id:"002"},
+    {name:"小",ischoose:false,_id:"003"},
+];
+let Cacheinsert="";
+let Cachesize="";
 Component({
     /**
      * 组件的属性列表
@@ -18,10 +30,11 @@ Component({
      */
     data: {
         fileList:[],
-        itemclass:"item",
         insert:"",
         size:"",
-        insertNum:1
+        insertNum:1,
+        insertList:[],
+        sizeList:[],
     },
 
     /**
@@ -48,25 +61,56 @@ Component({
         //点击选择虫子种类
         selectinsert(event)
         {
-            console.log(event);
-            console.log("点击了！",event.currentTarget.dataset.value);
-            this.setData({insert:event.currentTarget.dataset.value});
-            console.log("insert:",this.data.insert);
+            let value=event.currentTarget.dataset.value;
+            Cacheinsert=value;
+            CacheinsertList.forEach((element)=>{
+                if(element.name==value){element.ischoose=true}
+                else{element.ischoose=false}
+            })
+            this.setData({
+                insertList:CacheinsertList,
+                insert:event.currentTarget.dataset.value
+            });
         },
         // 点击选择虫子大小
         selectsize(event)
         {
-            console.log("又点击了！",event.currentTarget.dataset.value);
-            this.setData({size:event.currentTarget.dataset.value});
-            console.log("size:",this.data.size);
+            let value=event.currentTarget.dataset.value;
+            Cachesize=value;
+            let CachesizeList=this.data.sizeList;
+            CachesizeList.forEach((element)=>{
+                if(element.name==value){element.ischoose=true}
+                else{element.ischoose=false}
+            })
+            this.setData({
+                sizeList:CachesizeList,
+                size:value
+            })
         },
         // 点击保存按钮，实现组件间通信
         savebutton()
         {
+            // todo:保存前要做个校验
+            if(Cacheinsert=="")
+            {
+                console.log("虫子种类不能为空！")
+                return;
+            }
+            else if(Cachesize=="")
+            {
+                console.log("虫子大小不能为空！");
+                return;
+            }
+            else
+            {
+                console.log("选择完毕！");
+                
+            }
+            // 传出去的数据
             let passobj={
-                insert:this.data.insert,
-                size:this.data.size,
-                num:this.data.insertNum
+                insert:Cacheinsert,
+                size:Cachesize,
+                num:Cachenum
             }
             // 触发save事件，并将数据传出去
             this.triggerEvent("save",passobj);
@@ -84,8 +128,19 @@ Component({
         attached()
         {
             // 该组件进入到页面节点树时，更新上一次选择的数据，实现数据缓存效果
-            this.setData({insertNum:Cachenum,fileList:CachefileList});
-            console.log("组件进入页面节点树后执行，insertnum：",this.data.insertNum,this.data.fileList);
+            this.setData({
+                insertNum:Cachenum,
+                fileList:CachefileList,
+                insertList:CacheinsertList,
+                sizeList:CachesizeList
+            });
+            console.log("组件进入页面节点树后执行，insertnum：",this.data.insertNum);
+            console.log("fileList",this.data.fileList);
+            console.log("sizeList",this.data.sizeList);
+            console.log("insertList",this.data.insertList);
+            console.log("cacheinsert:",Cacheinsert);
+            console.log("cachesize:",Cachesize);
+            console.log("cachenum:",Cachenum);
         },
         
     }
