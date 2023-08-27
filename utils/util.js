23662f7972b074efff582b64aa1d2b0ed474d33a
Tohka <1324419148@qq.com>
2023-08-28 00:01:49 +0800

    添加方法:防抖函数，验证手机合法

--- utils/util.js
+++ utils/util.js
@@ -1,23 +1,43 @@
-const formatTime = date => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
-
-  return `${[year, month, day].map(formatNumber).join('/')} ${[hour, minute, second].map(formatNumber).join(':')}`
-}
-
-const formatNumber = n => {
-  n = n.toString()
-  return n[1] ? n : `0${n}`
-}
 // 格式化虫子信息
 const formatInformation=(passdata)=>{
     return passdata.insert+" "+passdata.size+" "+passdata.num+"只";
 }
 
+//函数防抖
+function debounce(func,time=1000)
+{
+    let timer;
+    return function(...params){
+        let context=this;
+        let args=params;
+        clearTimeout(timer);
+        timer=setTimeout(()=>{
+            func.apply(context,args);
+        },time);
+    }
+}
+
+//验证手机号码是否合法
+function VerifyPhone(...phoneIfo)
+{
+    console.log("验证手机号码接受到的参数",phoneIfo);
+    let phone=phoneIfo[0];//要验证的手机号码
+    const phoneNumberRegex = /^1[3-9]\d{9}$/;
+    if(phoneNumberRegex.test(phone))
+    {
+        console.log("合法！");
+        return true;
+    }
+    else
+    {
+        console.log("不合法！");
+        return false;
+    }
+}
+
+
 module.exports = {
-  formatInformation
+  formatInformation,
+  debounce,
+  VerifyPhone
 }
